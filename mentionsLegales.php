f52531bb8465b2fa03908a6d8b7590d7d9dd697f
GuillaumeMaurin <guillaumemaurin.gm@gmail.com>
2019-01-10 12:32:45 +0100

    add mentions legales

---
+++ mentionsLegales.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Espace Natal</title>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="/espace-natal/style.css">
+</head>
+
+<body>
+
+  <!-- NAVBAR -->
+  <?php include("views/navbar.html"); ?>
+  <div class="container my-5">
+    <h2><strong class="primaryColor">Mentions Légales</strong></h2>
+    <p><small>www.espace-natal.com est la propriété de SCM Centre Jean Richepin, Société Civile de Moyens au capital social de 5 250 euros. SCM Centre Jean Richepin est immatriculée au Registre du Commerce et des Sociétés de Paris sous le numéro 809
+        029
+        812.<br>
+        L’utilisateur de ce site s’engage à se conformer aux présentes conditions d’utilisation.</small></p>
+
+    <small><strong>Présentation du site :</strong></small>
+    <p><small>En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site de www.espace-natal.com , l’identité des différents intervenants dans le cadre de sa
+        réalisation et de son suivi :
+      </small></p>
+    <small><strong>Propriétaire :</strong></small>
+    <p><small>SCM Centre Jean Richepin, 8 rue Jean Richepin 75116 Paris
+        <br>Tél : 01.45.03.53.23
+        <br>Mail : contact@espace-natal.com
+      </small></p>
+    <small><strong>Directeur de publication :</strong></small>
+    <p><small>Salomé Azran
+      </small></p>
+    <small><strong>Hébergement :</strong></small>
+    <p><small>www.espace-natal.com est hébergé au sein de la société EWORLD CONCEPT, 6 place de la Madeleine, 75008 Paris, France. Enregistrée au Registre du Commerce et des Sociétés de Paris sous le numéro RCS Paris B 478 376 130
+      </small></p>
+    <small><strong>Propriété intellectuelle et contrefaçon :</strong></small>
+    <p><small>1) Les marques, logos et appellations ainsi que la structure générale, la présentation, et les contenus du site et plus généralement chacun des éléments créés pour ce site sont la propriété exclusive de SCM Centre Jean Richepin.<br>
+        Toute reproduction, toute représentation totale ou partielle, toute utilisation, toute adaptation, toute mise à disposition ou modification de ces éléments par quelque procédé que ce soit, par quelque personne que ce soit et par quelque
+        moyen
+        que ce soit (notamment la vente, la commercialisation, la location, etc.) sans l’autorisation expresse de SCM Centre Jean Richepin est strictement interdite et constitue un délit de contrefaçon au sens du code de la propriété intellectuelle
+        ou
+        un acte de concurrence déloyale.
+        <br>
+        2) Les reproductions iconographiques et photographiques présentés dans ce site sont protégés au titre du droit de la propriété intellectuelle.
+      </small></p>
+    <small><strong>Contenu du Site :</strong></small>
+    <p><small>Les informations présentes dans ce site ont pour objectif d’être utiles et informatives pour les clients et les investisseurs de SCM Centre Jean Richepin. SCM Centre Jean Richepin s’efforcera de faire en sorte que les informations
+        publiées
+        sur le site soient correctes au moment de leur mise en ligne. Cependant SCM Centre Jean Richepin ne peut certifier que les informations soient exactes et à jour en toute circonstance. De plus, SCM Centre Jean Richepin n’est en aucun cas
+        responsable d’éventuels dommages ou pertes que chacun peut subir en ayant consulté ces informations. Par ailleurs, SCM Centre Jean Richepin se réserve le droit de changer les informations.
+      </small></p>
+    <small><strong>Transparence et politique publicitaire et éditoriale :</strong></small>
+    <p><small>www.espace-natal.com ne reçoit aucun fond publicitaire de toute entreprise ayant un lien/intérêt avec la santé. Le site n’a pas pour objet de promouvoir des produits de santé quels qu’ils soient.
+      </small></p>
+    <small><strong>Sanction contre les atteintes au site :</strong></small>
+    <p><small>Nous rappelons que les lois pénales en vigueur relatives à la sécurité informatique sanctionnent l’intrusion frauduleuse, le maintien non autorisé dans un système, l’entrave volontaire au fonctionnement du système, à l’action
+        frauduleuse sur
+        les données.
+      </small></p>
+    <small><strong>Hyperliens :</strong></small>
+    <p><small>Le site peut inclure des liens vers d’autres sites Web ou d’autres sources Internet. Dans la mesure où SCM Centre Jean Richepin ne peut contrôler ces sites et ces sources externes, SCM Centre Jean Richepin ne peut encourir aucune
+        responsabilité quant au contenu, publicités, cookies, produits, services ou tout autre matériel disponible sur ou à partir de ces sites ou sources externes. De plus, SCM Centre Jean Richepin ne pourra être tenu responsable de tous dommages
+        ou
+        pertes avérés ou allégués consécutifs ou en relation avec l’utilisation ou avec le fait d’avoir fait confiance au contenu, à des biens ou des services disponibles sur ces sites ou sources externes.
+      </small></p>
+    <small><strong>Données collectées :</strong></small>
+    <p><small>A chacune de vos connexions sur notre site, nous avons la possibilité de récupérer des informations nous permettant de mieux vous connaître, vous comprendre et répondre à vos attentes et besoins.
+        <br><br>
+        Si vous ne souhaitez pas que ces données soient collectées, vous pouvez configurer votre explorateur de manière à ce qu’il rejette toute installation de fichiers (« cookies ») nous permettant de les obtenir. Le refus d’installation ou la
+        suppression de cookies n’aura aucune influence sur l’accès aux pages du site.
+        <br><br>
+        Par ailleurs, à l’occasion de prise de contact avec nous, vous avez la possibilité de nous laisser vos coordonnées, des informations personnelles vous concernant, ainsi que de souscrire à une newsletter.
+        <br><br>
+        Nous tenons ces informations strictement confidentielles et ne les utilisons qu’à notre usage exclusif. Vous pouvez bien entendu ne laisser aucune information, nous empêchant par la même de vous contacter.
+        <br><br>
+        Si vous souhaitez nous laisser ces informations, nous vous informons que conformément à la Loi Informatique et Libertés du 6 janvier 1978, vous disposez à tout moment d’un droit d’accès, de rectification ou de suppression des données vous
+        concernant en nous envoyant un e-mail à l’adresse suivante : contact@espace-natal.com
+      </small></p>
+    <small><strong>Environnement Technique :</strong></small>
+    <p><small>SCM Centre Jean Richepin décline toute responsabilité pour les difficultés rencontrées lors de l’accès à son site ou pour toute défaillance de communication.</small></p>
+
+  </div>
+  <?php include("views/footer.html"); ?>
+
+  <!-- Bootstrap js/jquery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+
+</html>
